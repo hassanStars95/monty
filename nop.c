43509073ee7bc4ebab9e8a6ad43709d46d3f8c03
hassanStars95 <seiduhassan36@gmail.com>
2023-01-25 13:00:19 -0800

    monty task

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing
+  *@head: head
+  *@counter: counts number of elements
+  *Return: no value
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
